f911e57d4240fddd15b7f48643234d04f6764c3a
Sarah Clark <sarahireneclark@gmail.com>
2024-03-07 15:10:14 -0600

    Added a view modifier for the row view

---
+++ PetPurrsuit/PetPurrsuit/AnimalsNearYou/Views/AnimalAttributesCard.swift
@@ -0,0 +1,21 @@
+//
+//  AnimalAttributesCard.swift
+//  PetPurrsuit
+//
+//  Created by Sarah Clark on 3/7/24.
+//
+
+import SwiftUI
+
+struct AnimalAttributesCard: ViewModifier {
+    let color: Color
+
+    func body(content: Content) -> some View {
+        content
+            .padding(4)
+            .background(color.opacity(0.2))
+            .cornerRadius(8)
+            .foregroundColor(color)
+            .font(.subheadline)
+    }
+}
