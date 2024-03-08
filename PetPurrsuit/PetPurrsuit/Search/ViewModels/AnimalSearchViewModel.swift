c4a30b0341cf458d5dba7611e69605a4c9b083ab
Sarah Clark <sarahireneclark@gmail.com>
2024-03-08 13:24:07 -0600

    Added code to search the petfinder rest api

--- PetPurrsuit/PetPurrsuit/Search/ViewModels/AnimalSearchViewModel.swift
+++ PetPurrsuit/PetPurrsuit/Search/ViewModels/AnimalSearchViewModel.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 final class AnimalSearchViewModel: ObservableObject {
     @Published var searchText = ""
+    private let animalSearcher: AnimalSearcher
+    private let animalStore: AnimalStore
+
+    init(animalSearcher: AnimalSearcher, animalStore: AnimalStore) {
+        self.animalSearcher = animalSearcher
+        self.animalStore = animalStore
+    }
 
     var shouldFilter: Bool {
         !searchText.isEmpty
+    }
+
+    func search() {
+        Task {
+            let animals = await animalSearcher.searchAnimal(by: searchText, age: .none, type: .none)
+
+            do {
+                try await animalStore.save(animals: animals)
+            } catch {
+                print("Error storing animals... \(error.localizedDescription)")
+            }
+        }
     }
 }
--- PetPurrsuit/PetPurrsuit/Search/Views/SearchAnimalsView.swift
+++ PetPurrsuit/PetPurrsuit/Search/Views/SearchAnimalsView.swift
@@ -11,14 +11,17 @@ struct SearchAnimalsView: View {
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \AnimalEntity.timestamp, ascending: true)],
         animation: .default)
     private var animals: FetchedResults<AnimalEntity>
-    @State var searchText = ""
+    @StateObject var viewModel = AnimalSearchViewModel(
+        animalSearcher: AnimalSearcherService(requestManager: RequestManager()),
+        animalStore: AnimalStoreService(context: PersistenceController.shared.container.newBackgroundContext()))
 
     var filteredAnimals: [AnimalEntity] {
-        animals.filter {
-            if searchText.isEmpty {
+        guard viewModel.shouldFilter else { return [] }
+        return animals.filter {
+            if viewModel.searchText.isEmpty {
                 return true
             }
-            return $0.name?.contains(searchText) ?? false
+            return $0.name?.contains(viewModel.searchText) ?? false
         }
     }
 
@@ -26,9 +29,12 @@ struct SearchAnimalsView: View {
         NavigationView {
             AnimalListView(animals: filteredAnimals)
                 .searchable(
-                    text: $searchText,
+                    text: $viewModel.searchText,
                     placement: .navigationBarDrawer(displayMode: .always)
                 )
+                .onChange(of: viewModel.searchText) { _ in
+                    viewModel.search()
+                }
                 .navigationTitle("Find your future pet")
         }
     }
