1ce4d07db2c190cf3a383449b4c17b78fd7e3072
Sarah Clark <sarahireneclark@gmail.com>
2024-03-07 19:03:06 -0600

    Added search functionality of locally stored Animals in CoreData

--- PetPurrsuit/PetPurrsuit/Search/Views/SearchAnimalsView.swift
+++ PetPurrsuit/PetPurrsuit/Search/Views/SearchAnimalsView.swift
@@ -11,10 +11,24 @@ struct SearchAnimalsView: View {
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \AnimalEntity.timestamp, ascending: true)],
         animation: .default)
     private var animals: FetchedResults<AnimalEntity>
+    @State var searchText = ""
+
+    var filteredAnimals: [AnimalEntity] {
+        animals.filter {
+            if searchText.isEmpty {
+                return true
+            }
+            return $0.name?.contains(searchText) ?? false
+        }
+    }
 
     var body: some View {
         NavigationView {
-            AnimalListView(animals: animals)
+            AnimalListView(animals: filteredAnimals)
+                .searchable(
+                    text: $searchText,
+                    placement: .navigationBarDrawer(displayMode: .always)
+                )
                 .navigationTitle("Find your future pet")
         }
     }
