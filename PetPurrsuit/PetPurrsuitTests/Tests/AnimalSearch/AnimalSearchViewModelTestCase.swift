b681a26e28396ee80a0070b8c06efa688f719522
Sarah Clark <sarahireneclark@gmail.com>
2024-03-08 19:10:11 -0600

    Added unit tests for the shouldFilter computed property in the AnimalSearchViewModel

---
+++ PetPurrsuit/PetPurrsuitTests/Tests/AnimalSearch/AnimalSearchViewModelTestCase.swift
@@ -0,0 +1,53 @@
+//
+//  AnimalSearchViewModelTestCase.swift
+//  PetPurrsuitTests
+//
+//  Created by Sarah Clark on 3/8/24.
+//
+
+import Foundation
+import XCTest
+@testable import PetPurrsuit
+
+final class AnimalSearchViewModelTestCase: XCTestCase {
+    let testContext = PersistenceController.preview.container.viewContext
+    var viewModel: AnimalSearchViewModel!
+
+    override func setUp() {
+        super.setUp()
+        viewModel = AnimalSearchViewModel(
+            animalSearcher: AnimalSearcherMock(),
+            animalStore: AnimalStoreService(context: testContext))
+    }
+
+    func testShouldFilterIsFalseForEmptyFilters() {
+        XCTAssertTrue(viewModel.searchText.isEmpty)
+        XCTAssertEqual(viewModel.ageSelection, .none)
+        XCTAssertEqual(viewModel.typeSelection, .none)
+        XCTAssertFalse(viewModel.shouldFilter)
+    }
+
+    func testShouldFilterIsTrueForSearchText() {
+        viewModel.searchText = "Kiki"
+        XCTAssertFalse(viewModel.searchText.isEmpty)
+        XCTAssertEqual(viewModel.ageSelection, .none)
+        XCTAssertEqual(viewModel.typeSelection, .none)
+        XCTAssertTrue(viewModel.shouldFilter)
+    }
+
+    func testShouldFilterIsTrueForAgeFilter() {
+        viewModel.ageSelection = .baby
+        XCTAssertTrue(viewModel.searchText.isEmpty)
+        XCTAssertEqual(viewModel.ageSelection, .baby)
+        XCTAssertEqual(viewModel.typeSelection, .none)
+        XCTAssertTrue(viewModel.shouldFilter)
+    }
+
+    func testShouldFilterIsTrueForTypeFilter() {
+        viewModel.typeSelection = .cat
+        XCTAssertTrue(viewModel.searchText.isEmpty)
+        XCTAssertEqual(viewModel.ageSelection, .none)
+        XCTAssertEqual(viewModel.typeSelection, .cat)
+        XCTAssertTrue(viewModel.shouldFilter)
+    }
+}
