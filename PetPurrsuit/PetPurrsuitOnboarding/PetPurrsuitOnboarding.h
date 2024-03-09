d5a6c70d3fd69f96c30336a13995e618cec40069
Sarah Clark <sarahireneclark@gmail.com>
2024-03-09 06:38:14 -0600

    Added new PetPurrsuitOnboarding framework target and it's related tests

---
+++ PetPurrsuit/PetPurrsuitOnboarding/PetPurrsuitOnboarding.h
@@ -0,0 +1,18 @@
+//
+//  PetPurrsuitOnboarding.h
+//  PetPurrsuitOnboarding
+//
+//  Created by Sarah Clark on 3/9/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PetPurrsuitOnboarding.
+FOUNDATION_EXPORT double PetPurrsuitOnboardingVersionNumber;
+
+//! Project version string for PetPurrsuitOnboarding.
+FOUNDATION_EXPORT const unsigned char PetPurrsuitOnboardingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PetPurrsuitOnboarding/PublicHeader.h>
+
+
